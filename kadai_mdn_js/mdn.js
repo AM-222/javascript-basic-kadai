de29f8e5f095dd1f78a2b9fb19fcdb6a5c67fd4d
AM-222 <3er.1ovende@gmail.com>
2024-12-23 08:32:14 +0900

    課題「関数をMDNで調べて使ってみよう」を提出

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,11 @@
+//現在のタイムスタンプを取得
+const aaa = new Date();
+
+//タイムスタンプを加工して出力
+console.log(aaa.getFullYear()
+          + "年"
+          + aaa.getMonth()
+          + "月"
+          + aaa.getDate()
+          + "日"
+          );
