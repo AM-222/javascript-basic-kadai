fb3ce8135ca5782351057d9cec6ded427fd1f087
AM-222 <3er.1ovende@gmail.com>
2024-12-23 14:13:07 +0900

    再提出（取得した"月"を修正）

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -4,7 +4,7 @@ const aaa = new Date();
 //タイムスタンプを加工して出力
 console.log(aaa.getFullYear()
           + "年"
-          + aaa.getMonth()
+          + (aaa.getMonth()+1)
           + "月"
           + aaa.getDate()
           + "日"
